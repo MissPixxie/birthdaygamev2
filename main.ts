a723ce6f7b66f7851df2760e9449788c7dbefb12
Emelie Hansson <emeliehanssonn@hotmail.se>
2025-05-15 22:27:14 +0200

    more room maps

--- main.ts
+++ main.ts
@@ -6,11 +6,17 @@ import apartmentMapData from "./public/map2.json";
 import hallwayMapData from "./public/map3.json";
 import basementMapData from "./public/map4.json";
 import basementRoom1MapData from "./public/map5.json";
+import basementRoom2MapData from "./public/map6.json";
+import basementRoom3MapData from "./public/map7.json";
+import basementRoom4MapData from "./public/map8.json";
 import hallwayScene from "./src/scenes/hallwayScene.ts";
 import { loadSprites } from "./src/ui/loadSprites.ts";
 import basementScene from "./src/scenes/basementScene.ts";
 import "./src/utils/sidePanelLogic.ts";
 import basementRoom1Scene from "./src/scenes/basementRoom1Scene.ts";
+import basementRoom2Scene from "./src/scenes/basementRoom2Scene.ts";
+import basementRoom3Scene from "./src/scenes/basementRoom3Scene.ts";
+import basementRoom4Scene from "./src/scenes/basementRoom4Scene.ts";
 // import { getCurrentActivePanel } from "./src/utils/uiManager.ts";
 // import { closeBackpack, closeHelp } from "./src/utils/sidePanelLogic.ts";
 // import { closeDialogue } from "./src/utils/dialogueLogic.ts";
@@ -29,6 +35,18 @@ kaBoom.scene("basementScene", (previousSceneData) => {
 
 kaBoom.scene("basementRoom1Scene", (previousSceneData) => {
 	basementRoom1Scene(kaBoom, basementRoom1MapData, previousSceneData);
+});
+
+kaBoom.scene("basementRoom2Scene", (previousSceneData) => {
+	basementRoom2Scene(kaBoom, basementRoom1MapData, previousSceneData);
+});
+
+kaBoom.scene("basementRoom3Scene", (previousSceneData) => {
+	basementRoom3Scene(kaBoom, basementRoom1MapData, previousSceneData);
+});
+
+kaBoom.scene("basementRoom4Scene", (previousSceneData) => {
+	basementRoom4Scene(kaBoom, basementRoom1MapData, previousSceneData);
 });
 
 loadSprites().then(() => {
--- src/ui/loadSprites.ts
+++ src/ui/loadSprites.ts
@@ -80,6 +80,9 @@ export async function loadSprites() {
 		await kaBoom.loadSprite("hallwayMap", "../map3.png");
 		await kaBoom.loadSprite("basementMap", "../map4.png");
 		await kaBoom.loadSprite("basementRoom1Map", "../map5.png");
+		await kaBoom.loadSprite("basementRoom2Map", "../map5.png");
+		await kaBoom.loadSprite("basementRoom3Map", "../map5.png");
+		await kaBoom.loadSprite("basementRoom4Map", "../map5.png");
 		console.log("map sprites laddade!");
 
 		console.log("Laddar bullet sprite...");
